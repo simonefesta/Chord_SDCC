fc9a498cd4e97714cce7eb9475fa5a19f5f70d46
simonefesta <simonefesta8@gmail.com>
2023-09-19 13:45:49 +0200

    [Fine Progetto] - Aggiornamento slide

--- node/utils.go
+++ node/utils.go
@@ -176,7 +176,7 @@ func InterrogateFinger(arg *Arg, reply *string) string {
 	for i := 0; i < len(node.Finger)-1; i++ { //ispeziono FT
 
 		if ((idRisorsa > node.Finger[i]) && (idRisorsa <= node.Finger[i+1])) || (node.Finger[i] > node.Finger[i+1] && idRisorsa <= node.Finger[i+1]) {
-			nodoContactId = node.Finger[i+1]
+			nodoContactId = node.Finger[i] //il nodo più lontano che non eccede la risorsa.
 			isFound = true
 			break
 		}
